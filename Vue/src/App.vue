1b6b396ec5b38ab22038b67b72fe9d70df524a3f
Khoi Nguyen Minh <41194212+TehCoesy@users.noreply.github.com>
2023-08-28 16:40:45 +0200

    Start of Vue.js Project
    Implemented main structure and sample of Vue.js project.

---
+++ Vue/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import SimpleButton from './components/SimpleButton.vue';
+
+export default {
+    data() {
+        return {
+            mssg: 'This is dynamic message'
+        };
+    },
+    components: { SimpleButton }
+}
+</script>
+
+<template>
+  <div>
+    <h1> Hello, welcome to Vue! </h1>
+    <h2> {{ mssg }} </h2>
+    <SimpleButton></SimpleButton>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ Vue/src/components/SimpleButton.vue
@@ -0,0 +1,18 @@
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const count = ref(0)
+        return { count }
+    }
+}
+</script>
+
+<template>
+    <button @click="count++">Clicked {{ count }} times</button>
+</template>
+
+<style scoped>
+
+</style>
